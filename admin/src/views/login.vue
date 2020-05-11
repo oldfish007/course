c05729888d3f82ff51431f70521abb5f136a26eb
466403678@qq.com <zhengxinhai711@163.com>
2020-05-11 10:38:03 +0800

    1.增加admin页面 2.增加路由配置,访问/admin页面，加载admin组件

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -1,120 +1,120 @@
 <template>
-    <div class="main-container">
-        <div class="main-content">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="login-container">
-                        <div class="center">
-                            <h1>
-                                <i class="ace-icon fa fa-leaf green"></i>
-                                <span class="">控台登录</span>
-                            </h1>
-                        </div>
+  <div class="main-container">
+    <div class="main-content">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="login-container">
+            <div class="center">
+              <h1>
+                <i class="ace-icon fa fa-leaf green"></i>
+                <span class="">控台登录</span>
+              </h1>
+            </div>
 
-                        <div class="space-6"></div>
+            <div class="space-6"></div>
 
-                        <div class="position-relative">
-                            <div id="login-box" class="login-box visible widget-box no-border">
-                                <div class="widget-body">
-                                    <div class="widget-main">
-                                        <h4 class="header blue lighter bigger">
-                                            <i class="ace-icon fa fa-coffee green"></i>
-                                            请输入用户名密码
-                                        </h4>
+            <div class="position-relative">
+              <div id="login-box" class="login-box visible widget-box no-border">
+                <div class="widget-body">
+                  <div class="widget-main">
+                    <h4 class="header blue lighter bigger">
+                      <i class="ace-icon fa fa-coffee green"></i>
+                      请输入用户名密码
+                    </h4>
 
-                                        <div class="space-6"></div>
+                    <div class="space-6"></div>
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
+                    <form>
+                      <fieldset>
+                        <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
 															<input type="text" class="form-control"
-                                                                   placeholder="用户名"/>
+                                     placeholder="用户名"/>
 															<i class="ace-icon fa fa-user"></i>
 														</span>
-                                                </label>
+                        </label>
 
-                                                <label class="block clearfix">
+                        <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
 															<input type="password" class="form-control"
-                                                                   placeholder="密码"/>
+                                     placeholder="密码"/>
 															<i class="ace-icon fa fa-lock"></i>
 														</span>
-                                                </label>
+                        </label>
 
-                                                <div class="space"></div>
+                        <div class="space"></div>
 
-                                                <div class="clearfix">
-                                                    <label class="inline">
-                                                        <input v-model="remember" type="checkbox" class="ace"/>
-                                                        <span class="lbl">记住我</span>
-                                                    </label>
+                        <div class="clearfix">
+                          <label class="inline">
+                            <input v-model="remember" type="checkbox" class="ace"/>
+                            <span class="lbl">记住我</span>
+                          </label>
 
-                                                    <button type="button"
-                                                            class="width-35 pull-right btn btn-sm btn-primary">
-                                                        <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">登录</span>
-                                                    </button>
-                                                </div>
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary">
+                            <i class="ace-icon fa fa-key"></i>
+                            <span class="bigger-110">登录</span>
+                          </button>
+                        </div>
 
-                                                <div class="space-4"></div>
-                                            </fieldset>
-                                        </form>
+                        <div class="space-4"></div>
+                      </fieldset>
+                    </form>
 
-                                        <div class="social-or-login center">
-                                            <span class="bigger-110">Or Login Using</span>
-                                        </div>
+                    <div class="social-or-login center">
+                      <span class="bigger-110">Or Login Using</span>
+                    </div>
 
-                                        <div class="space-6"></div>
+                    <div class="space-6"></div>
 
 
-                                    </div><!-- /.widget-main -->
-                                </div><!-- /.widget-body -->
-                            </div><!-- /.login-box -->
+                  </div><!-- /.widget-main -->
+                </div><!-- /.widget-body -->
+              </div><!-- /.login-box -->
 
-                            <div id="forgot-box" class="forgot-box widget-box no-border">
-                                <div class="widget-body">
-                                    <div class="widget-main">
-                                        <h4 class="header red lighter bigger">
-                                            <i class="ace-icon fa fa-key"></i>
-                                            Retrieve Password
-                                        </h4>
+              <div id="forgot-box" class="forgot-box widget-box no-border">
+                <div class="widget-body">
+                  <div class="widget-main">
+                    <h4 class="header red lighter bigger">
+                      <i class="ace-icon fa fa-key"></i>
+                      Retrieve Password
+                    </h4>
 
-                                        <div class="space-6"></div>
-                                        <p>
-                                            Enter your email and to receive instructions
-                                        </p>
+                    <div class="space-6"></div>
+                    <p>
+                      Enter your email and to receive instructions
+                    </p>
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
+                    <form>
+                      <fieldset>
+                        <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
 															<input type="email" class="form-control"
-                                                                   placeholder="Email"/>
+                                     placeholder="Email"/>
 															<i class="ace-icon fa fa-envelope"></i>
 														</span>
-                                                </label>
+                        </label>
 
-                                                <div class="clearfix">
-                                                    <button type="button"
-                                                            class="width-35 pull-right btn btn-sm btn-danger">
-                                                        <i class="ace-icon fa fa-lightbulb-o"></i>
-                                                        <span class="bigger-110">Send Me!</span>
-                                                    </button>
-                                                </div>
-                                            </fieldset>
-                                        </form>
-                                    </div><!-- /.widget-main -->
+                        <div class="clearfix">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-danger">
+                            <i class="ace-icon fa fa-lightbulb-o"></i>
+                            <span class="bigger-110">Send Me!</span>
+                          </button>
+                        </div>
+                      </fieldset>
+                    </form>
+                  </div><!-- /.widget-main -->
 
-                                </div><!-- /.widget-body -->
-                            </div><!-- /.login-box -->
-                        </div><!-- /.position-relative -->
+                </div><!-- /.widget-body -->
+              </div><!-- /.login-box -->
+            </div><!-- /.position-relative -->
 
-                    </div>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.main-content -->
-    </div><!-- /.main-container -->
+          </div>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.main-content -->
+  </div><!-- /.main-container -->
 </template>
 
 <script>
